005b474b71076d9cdea94682888ba435578e2bcf
JohnBrandt <jbrandt@ggc.edu>
2021-08-29 19:39:45 -0400

    Added the second width x height file

---
+++ 02-add_nums_JohnBrandt.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+sum=$(( first+second+third ))
+
+echo "The sum is $sum"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
+
+#John Brandt's exercise
+
+echo "Please enter the width"
+read width
+echo "Please enter the height"
+read height
+expr $width \* $height
